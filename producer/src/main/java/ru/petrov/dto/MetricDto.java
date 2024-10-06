1855aea2fa21b6984a74baa86e54d144d544296d
Aleksei Petrov <peta0982@gmail.com>
2024-10-06 19:42:47 +0500

    description @Schema MetricDto

--- producer/src/main/java/ru/petrov/dto/MetricDto.java
+++ producer/src/main/java/ru/petrov/dto/MetricDto.java
@@ -1,5 +1,6 @@
 package ru.petrov.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,12 +14,21 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class MetricDto {
     @NotNull
+    @Schema(description = "Id замера")
+    private Long id;
+    @NotNull
+    @Schema(description = "Дата и время")
     private LocalDateTime localDateTime;
     @Size(min = 2, max = 255, message = "name must be from 2 to 255 chars")
+    @Schema(description = "Имя метрики", example = "Uptime")
     private String name;
+    @NotNull
+    @Schema(description = "Описание метрики", example = "Время с момента запуска приложения")
     private String description;
     @NotNull
+    @Schema(description = "Единица измерения", example = "ms")
     private String baseUnit;
     @NotNull
+    @Schema(description = "Значение замера", example = "1000")
     private String measurement;
 }
